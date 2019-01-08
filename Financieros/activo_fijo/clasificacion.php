0eb13c85f218966963f7f0bb10e2f03f09ef618f
Yancifany <yancifany@gmail.com>
2019-01-08 13:02:06 -0600

    cladicicacion

--- Financieros/activo_fijo/clasificacion.php
+++ Financieros/activo_fijo/clasificacion.php
@@ -19,7 +19,7 @@ if (isset($_REQUEST['nameEnviar'])) {
     
     Conexion::abrir_conexion();
     
-    ////////esto es para guardar persona juridica
+    //////esto es para guardarçç persona juridica
     $juridica = new persona_juridica();
     $juridica->setId_nombre($_REQUEST['nameNombre']);
     $juridica->setNumero($_REQUEST['nameNumero']);
